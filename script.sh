885ca227ec3587c55df7b23dae59afdcb4aa6262
Monish Basaniwal <46259712+monizb@users.noreply.github.com>
2020-12-13 21:09:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,25 +1,32 @@
 #! /bin/bash
-
+#Cloud Storage Lab
 #Initializing Configuration
 gcloud init < a
 
+cookie='replace with ur cookie'
 ID=$(gcloud info --format='value(config.project)')
+id=$(curl -X GET --header "cookie: $cookie" "https://www.qwiklabs.com/focuses/show_spl/6485.json?parent=game" | jq -r '.labInstanceId')
+echo $id
 
 # Create a GCS Bucket
 if  gsutil mb gs://$ID
 then
-  printf "\n\e[1;96m%s\n\n\e[m" 'Created Bucket: Checkpoint Completed (1/3)'
+  result1=$(curl -X GET --header "cookie: $cookie" "https://www.qwiklabs.com/assessments/run_step.json?id=$id&step=1")
 
   # Copy an object to a folder in the bucket (ada.jpg)
-  if  gsutil cp ./ada.jpg gs://$ID/image-folder/ada.jpg
+  if  gsutil cp ./ada.jpg gs://$ID/
   then
-    printf "\n\e[1;96m%s\n\n\e[m" 'File Copied: Checkpoint Completed (2/3)'
+    result2=$(curl -X GET --header "cookie: $cookie" "https://www.qwiklabs.com/assessments/run_step.json?id=$id&step=2")
 
     # Make your object publicly accessible
-    if  gsutil acl ch -u AllUsers:R gs://$ID/image-folder/ada.jpg
+    if  gsutil acl ch -u AllUsers:R gs://$ID/ada.jpg
     then
-      printf "\n\e[1;96m%s\n\n\e[m" 'Changed Permission: Checkpoint Completed (3/3)'
+      result3=$(curl -X GET --header "cookie: $cookie" "https://www.qwiklabs.com/assessments/run_step.json?id=$id&step=3")
 
+      quiz1=$(curl -X POST --header "cookie: $cookie" --data "{"stem":"Every bucket must have a unique name across the entire Cloud Storage namespace.","is_correct":true,"response":"{\"value\":true}","item_type":"true-false"}" "https://www.qwiklabs.com/lab_instances/$id/probe_responses")
+      quiz2=$(curl -X POST --header "cookie: $cookie" --data "{"stem":"Cloud Storage offers which storage classes:","is_correct":true,"response":"[{\"id\":\"2\",\"title\":\"Coldline\"},{\"id\":\"0\",\"title\":\"Standard\"},{\"id\":\"3\",\"title\":\"Archive\"},{\"id\":\"1\",\"title\":\"Nearline\"}]","item_type":"multiple-select"}" "https://www.qwiklabs.com/lab_instances/$id/probe_responses")
+      quiz3=$(curl -X POST --header "cookie: $cookie" --data "{"stem":"Object names must be unique only within a given bucket.","is_correct":true,"response":"{\"value\":true}","item_type":"true-false"}" "https://www.qwiklabs.com/lab_instances/$id/probe_responses")
+      
       printf "\n\e[1;92m%s\n\n\e[m" 'Lab Completed'
     fi
   fi
